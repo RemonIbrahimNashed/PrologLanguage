f271bd8ed8a11a788a596d6f75208268d8888321
RemonIbrahimNashed <remon.inashed@gmail.com>
2019-04-20 20:53:02 +0200

    add switch stmt

--- CompilerDesignUsingProlog/compiler_2.pl
+++ CompilerDesignUsingProlog/compiler_2.pl
@@ -45,20 +45,20 @@ main(SourceCode):-
     atom_chars(SourceCode, TokenList) ,
     % tokenize_atom(SourceCode,TokenList),
     write(TokenList),
-    stmt(TokenList,[]) ,
+    % switch_case_stmt(TokenList,[]),
+    stmts(TokenList,[]) ,
     write("syntax free"), 
     ! ;
     write("syntax error").
 
-% means there are zero or more spaces
-skip -->  ([' '];['\t'];['\n'];['\r']), skip ; [] .
 
-stmt --> skip , assignment_stmt ,skip | skip,if_stmt,skip|skip, while_stmt ,skip| skip,for_stmt, skip|skip , do_while_stmt ,skip|skip , open_curlybracket ,skip, stmts ,skip,  close_curlybracket,skip.
+
+stmt --> skip , assignment_stmt ,skip|skip,switch_case_stmt,skip| skip,if_stmt,skip|skip, while_stmt ,skip| skip,for_stmt, skip|skip , do_while_stmt ,skip|skip , open_curlybracket ,skip, stmts ,skip,  close_curlybracket,skip.
 stmts --> skip ,stmt , skip ,stmts,skip | [] .
 
 assignment_stmt --> assignment_exp, skip , semicolon_op , skip |skip ,  postfix_exp , skip , semicolon_op , skip |skip ,  prefix_exp , skip , semicolon_op , skip .
 
-assignment_exp --> id ,skip, assignment_op ,skip, (exp|postfix_exp|prefix_exp) ,skip.
+assignment_exp --> id ,skip, assignment_op ,skip, (exp|postfix_exp|prefix_exp|string) ,skip.
 postfix_exp --> id ,increment_decrease_op , skip .
 prefix_exp --> increment_decrease_op , id , skip.
 
@@ -67,9 +67,15 @@ while_stmt --> skip , while_keyword ,skip , stmt_condition ,skip , stmt, skip.
 for_stmt --> skip , for_keyword , skip,for_condition ,skip, stmt,skip .
 do_while_stmt --> skip,do_keyword , skip,stmt ,skip, while_keyword , skip,stmt_condition ,skip, semicolon_op,skip .
 
+switch_case_stmt --> skip , switch_keyword , switch_case_condition , skip , switch_body , skip.
+switch_body --> skip,open_curlybracket,skip,case_stmts ,skip,(default_stmt|[]), skip,close_curlybracket.
+case_stmt --> skip , case_keyword ,required_space, skip, (string|digits) , skip, double_douts ,skip , stmts , skip , (break_stmt|[]), skip .
+break_stmt -->skip,break_keyword,skip,semicolon_op.
+default_stmt--> skip , default_keyword , skip , double_douts , skip , stmts , skip , (break_stmt|[]), skip .
+case_stmts --> skip,case_stmt , skip , case_stmts , skip | [].
+
 exp --> term , skip, rest .
 rest --> skip , plus_minus_op , skip , term ,skip, rest ; [] . 
-
  
 term --> factor ,skip , rest1 , skip .
 rest1 --> skip , multiplication_division_op ,skip, term ,skip, rest1 ,skip ; [] .
@@ -79,11 +85,15 @@ factor --> skip,digits,skip | skip,id,skip | skip,open_parenthesis,skip,exp,skip
 condition --> skip,factor,skip,relational_op,skip,factor,skip|skip,open_parenthesis,condition,close_parenthesis,skip,logical_op,skip,open_parenthesis,condition,close_parenthesis,skip.
 stmt_condition --> skip,open_parenthesis ,skip,condition ,skip, close_parenthesis,skip .
 for_condition --> skip,open_parenthesis,skip,(assignment_exp | [] ),skip,semicolon_op,skip,condition,skip,semicolon_op,skip,( postfix_exp | prefix_exp),skip,close_parenthesis,skip.
+switch_case_condition --> skip,open_parenthesis,skip,id,skip,close_parenthesis.
 
-
-
+% means there are zero or more spaces
+skip -->  ([' '];['\t'];['\n'];['\r']), skip ; [] .
+% required space between parts of the language
+required_space --> [' '].
 
 % terminals 
+
 digit --> ['0'];['1'];['2'];['3'];['4'];['5'];['6'];['7'];['8'];['9'].
 digits --> digit, digits ; digit .
 
@@ -92,40 +102,37 @@ letter --> ['A'];['B'];['C'];['D'];['E'];['F'];['G'];['H'];['I'];['J'];['K'];['L
 letter_or_digit --> letter ; digit .
 zero_or_more_letter_or_digit --> letter_or_digit , zero_or_more_letter_or_digit ; [] . 
 id --> letter , zero_or_more_letter_or_digit .
-    
+
+string --> quote,(id|digits) , quote .    
+
 % operators
 plus_minus_op --> [+];[-].
 multiplication_division_op --> [*] | [/] | ['%'].  
 
 relational_op --> [>,=] ; [<,=] ; [<] ; [>] ; [=,=] ; [!,=].
 logical_op --> ['&','&'] | ['|','|'] . 
+
 assignment_op --> [=] .
 increment_decrease_op --> ['+','+'] | ['-','-']. 
+
 semicolon_op --> [;] .
 
-% used parenthesis and brackets 
+% used symbols
 open_parenthesis --> ['('].
 close_parenthesis --> [')'] .
 open_curlybracket -->['{'].
 close_curlybracket --> ['}'].
+double_douts --> [':'].
+cout_op -->['<','<'].
+quote -->['"']|['\''].
 
 % key words 
 if_keyword --> ['i','f'].
 while_keyword --> ['w','h','i','l','e'] .
 for_keyword --> ['f','o','r'].
 do_keyword --> ['d','o'].
-
-% A = [i,f,' ',' ',' ','(',' ',x]
-% remove_space([],[]).
-% remove_space([H|T],[H1|T1]):-
-%     H \== ' ', 
-%     H1 = H ,
-%     remove_space(T,T1) .
-
-% digits([H|T],T):-
-%     with_output_to(atom(A),write(H)),
-%     re_match('[0-9]+',A).
-
-% id([H|T],T) :- 
-%     with_output_to(atom(A),write(H)),
-%     re_match("^([a-zA-z])([a-zA-Z0-9]*)",A).
+switch_keyword -->['s','w','i','t','c','h'].
+case_keyword -->['c','a','s','e'].
+print_keyword --> ['c','o','u','t'].
+break_keyword --> ['b','r','e','a','k'].
+default_keyword -->['d','e','f','a','u','l','t'].
